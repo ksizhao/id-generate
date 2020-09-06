c37d721344927ac02b26917ac9626816d68e0de8
br <17816873148@163.com>
2020-09-06 16:57:33 +0800

    增加消费者

---
+++ id-generate-customer/src/main/java/com/fingard/idgeneratecustomer/config/ConsumerConfiguration.java
@@ -0,0 +1,11 @@
+package com.fingard.idgeneratecustomer.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fingard.idgeneratecustomer.config")
+public class ConsumerConfiguration {
+
+
+}
---
+++ id-generate-customer/src/main/java/com/fingard/idgeneratecustomer/service/GenarateService.java
@@ -0,0 +1,16 @@
+package com.fingard.idgeneratecustomer.service;
+
+import fingard.entity.Result;
+
+/**
+ * @author zhaolc
+ * @version 1.0
+ * @description TODO
+ * @createTime 2020年08月21日 11:23:00
+ */
+public interface GenarateService {
+
+
+    Result getID();
+
+}
